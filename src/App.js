17720da2d32e166638e287512915ba1424ec3936
Sonyei <esonyei@gmail.com>
2020-10-16 12:15:25 -0400

    form rendering and accepting checkboxes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Link } from 'react-router-dom'
 import Form from './Pizza'
 import Home from './Home'
 
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  toppings: {
+    cheese: false,
+    pepp: false,
+    olive: false,
+    tomat: false
+  }
+}
+
+const initialFormErrors = {
+  name: '',
+  size: '',
+}
+
+const initialPizzas = []
+const initialDisabled = true;
+
+
 const App = () => {
+
+const [pizzas, setPizzas] = useState(initialPizzas)
+const [formValues, setFormValues] = useState(initialFormValues)
+const [formErrors, setFormErrors] = useState(initialFormErrors)
+const [disabled, setDisabled] = useState(initialDisabled)
+
+const formSubmit = () => {
+  const newPizza = {
+    name: formValues.name.trim(),
+    size: formValues.size,
+    toppings: ['cheese', 'pepp', 'olive', 'tomat'].filter(
+      (topping) => formValues[topping]
+    ),
+  }
+}
+
+const inputChange = () => {
+  
+}
+
   return (
     <>
     <div>
-      <div>
+      <div className='nav'>
         <h1>Lambda Eats</h1>
         <Link to="/">Home</Link>
         <Link to="/pizza">Order</Link>
@@ -19,7 +60,13 @@ const App = () => {
       
 
       <Route path='/pizza/'>
-        <Form/>
+        <Form
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+        />
       </Route>
 
        </div>
--- src/Pizza.js
+++ src/Pizza.js
@@ -2,13 +2,103 @@ import React from 'react'
 
 
 
-export default function Form() {
+export default function Form(props) {
+
+const { values, disabled, change, submit, errors } = props
+const { cheese, pepp, olive, tomat } = values
+
+
+const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
+}
+
+const onSubmit = (evt) => {
+    evt.preventDefault()
+    submit()
+}
+
 
 return (
     <div>
-
-    <button>Back to Home</button>
     
+        <form onSubmit={onSubmit}>
+        <h2>Create-A-Pizza Order Form</h2>
+        <div>{errors.name}</div>
+
+        <label> Name:
+            <input
+            name='name'
+            type='text'
+            value={values.name}
+            placeholder='Name goes here.'
+            onChange={onChange} />
+        </label>
+
+
+          <div>{errors.size}</div>
+        <label> Size 
+            <select
+            name='size'
+            value={values.size}
+            onChange={onChange}>
+                <option value="">--Choose a Size</option>
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+                <option value="el">Extra Large</option>
+            </select>
+       </label>
+        <br />
+
+        <div>
+        <h3>Choose your Toppings:</h3>
+       <label>
+            <input
+            name='cheese'
+            type='checkbox'
+            checked={values.cheese}
+            onChange={onChange}
+            />Cheese
+       </label>
+       <br />
+
+              <label>
+            <input
+            name='pepp'
+            type='checkbox'
+            checked={values.pepp}
+            onChange={onChange}
+            />Pepperoni
+       </label>
+       <br />
+
+              <label>
+            <input
+            name='olive'
+            type='checkbox'
+            checked={values.olive}
+            onChange={onChange}
+            />Olives
+       </label>
+       <br />
+
+              <label>
+            <input
+            name='tomat'
+            type='checkbox'
+            checked={values.tomat}
+            onChange={onChange}
+            />Tomatoes
+       </label>
+       <br />
+       </div>
+
+       <button disabled={disabled}>Place order</button>
+
+        </form>
+
     </div>
 )
 
