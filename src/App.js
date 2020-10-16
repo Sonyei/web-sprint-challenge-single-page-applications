9781fb2e6ae245795e30568a39c46f1ca7577fd5
Sonyei <esonyei@gmail.com>
2020-10-16 11:13:56 -0400

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+       <Route path='/'>Ordering Form</Route>
     </>
   );
 };
